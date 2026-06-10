a98c04fd6ba327fea98f5b54f05c5b9879655e88
Shivani Anajipuram <shivanianajipuram@gmail.com>
2026-06-10 22:56:02 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -74,19 +74,6 @@ async function start() {
     res.send(jobs);
   });
 
-  //FRONTEND 
-
-  const __dirname = path.resolve();
-
- app.use(express.static(path.join(__dirname, "public")));
-
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-  app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
   //START SERVER 
 
   app.listen(port, () => {
